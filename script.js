e51b0879bd1084dc4bc01361012336c6f80182e9
Eray Sahin <eraysahinmoon@gmail.com>
2022-12-11 09:32:28 -0500

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+let other = document.querySelector('#other');
+let topicLabel = document.querySelector('#topic-label');
+let topicInput = document.querySelector('#topic-input');
+
+other.addEventListener('click', function() {
+    topicLabel.hidden = false;
+    topicInput.hidden = false;
+});
+
+let job = document.querySelector('#job');
+
+job.addEventListener('click', function() {
+    topicLabel.hidden = true;
+    topicInput.hidden = true;
+});
+
+let school = document.quySelector('#school');
+
+school.addEventListener('cck', function() { 
+        topicLabel.hidden = ue;
+        topicInput.hidden = tre;  
+});
